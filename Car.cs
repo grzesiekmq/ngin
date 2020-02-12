8e37b4fdcb298f37b475cfcc277adc4bf88bfafa
grzesiekmq <grzesiekfuta@gmail.com>
2020-02-12 20:08:44 +0100

    added car and math

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace rgn
+{
+	public class Car
+	{
+
+		
+
+		
+
+		
+
+		float steerInput = 1;
+		/// <summary>
+		/// left angle of ackermann steering in degrees
+		/// </summary>
+		/// <param name="rearTrack"></param>
+		/// <param name="wheelBase"></param>
+		/// <param name="turnRadius"></param>
+		/// 
+		/// <returns>angle</returns>
+		public double AckermannLeft(float rearTrack, float wheelBase, float turnRadius)
+		{
+			return Math.RadToDeg(System.Math.Atan(wheelBase / (turnRadius + rearTrack / 2)) * steerInput);
+		}
+		/// <summary>
+		/// right angle of ackermann steering in degrees
+		/// </summary>
+		/// <param name="rearTrack"></param>
+		/// <param name="wheelBase"></param>
+		/// <param name="turnRadius"></param>
+		/// <returns>angle</returns>
+		public double AckermannRight(float rearTrack, float wheelBase, float turnRadius)
+		{
+			return Math.RadToDeg(System.Math.Atan(wheelBase / (turnRadius - rearTrack / 2)) * steerInput);
+		}
+
+
+
+
+	}
+}
---
+++ Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace rgn
+{
+    class Math
+    {
+        /// <summary>
+        /// convert radians to degrees
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns>converted angle</returns>
+        public static double RadToDeg(double angle)
+        {
+            return angle * (180.0 / System.Math.PI);
+        }
+    }
+}
