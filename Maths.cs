4c18f08e35644fff72910b2ecb0e791475f1ce2d
grzesiekmq <grzesiekfuta@gmail.com>
2020-02-16 20:52:45 +0100

    added maths

---
+++ Maths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace rgn
+{
+    public static class Maths
+    {
+        /// <summary>
+        /// convert radians to degrees
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns>converted angle</returns>
+        public static double RadToDeg(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        /// <summary>
+        /// Gets random item from array.
+        /// </summary>
+        /// <returns>The item.</returns>
+        /// <param name="array">Array.</param>
+        public static string RandomItem(string[] array)
+        {
+            var rand = new Random();
+            var max = array.Length;
+            var index = rand.Next(max);
+
+            var item = array[index];
+            return item;
+        }
+    }
+}
